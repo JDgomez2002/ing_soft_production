43d3fc156a617699fdcee1abff4be8c4478b0097
chamale-rac <samuel.chamale23@gmail.com>
2023-04-20 23:04:16 -0600

    Building routes

--- ing-soft-production/src/pages/index.jsx
+++ ing-soft-production/src/pages/index.jsx
@@ -4,12 +4,23 @@ import { Switch, Route } from 'react-router-dom'
 import MainPage from './MainPage'
 
 const Page = () => {
-
   return (
     <Switch>
+      {/*READY */}
       <Route path="/MainPage">
-        <MainPage/>
+        <MainPage />
       </Route>
+      {/*BUILDING */}
+      <Route path="/Login">
+        <p>Login</p>
+      </Route>
+      <Route path="/Signin">
+        <p>Signin</p>
+      </Route>
+      <Route path="/Landing">
+        <p>Signin</p>
+      </Route>
+      {/*MAIN */}
       <Route path="/">
         <Login />
       </Route>
